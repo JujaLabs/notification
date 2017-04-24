8a4f92c0e4d0894c429620d2f55f9c2d1bbc6cb4
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-04-24 19:08:58 +0300

    #1: add spring boot start

--- src/main/java/juja/microservices/notification/NotificationServiceStarter.java
+++ src/main/java/juja/microservices/notification/NotificationServiceStarter.java
@@ -1,8 +1,16 @@
 package juja.microservices.notification;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Danil Kuznetsov
  */
+
+@SpringBootApplication
 public class NotificationServiceStarter {
 
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceStarter.class, args);
+    }
 }
